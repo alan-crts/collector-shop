77aa6023aa685cbdfc791934a37add058c5f3a35
Alan Courtois <alan@wesur.fr>
2026-02-27 00:09:44 +0100

    feat: Integrate SonarCloud for static code analysis and coverage reporting in the backend CI pipeline.

--- apps/backend/jest.config.cjs
+++ apps/backend/jest.config.cjs
@@ -9,4 +9,13 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
   testPathIgnorePatterns: ["/node_modules/", "/dist/", "\\.d\\.ts$"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/__tests__/**"
+  ],
 };
